18a99d43c5d10bea501b5802299e0852ece4874d
freitas-andrew <freitas_andrew@outlook.com>
2025-04-20 21:43:58 +0100

    Changed samples around to provide better analysis later on, also importantly removed states.

--- scrapers/utils/data_sample.py
+++ scrapers/utils/data_sample.py
@@ -1,7 +1,6 @@
 # Data sample: modifying makes changes across both scrapers
 coverage_amounts = [100000, 250000, 350000, 500000, 750000, 1000000, 2000000, 5000000]
-term_lengths = ["10 Year Term", "20 Year Term", "30 Year Term"]
-states = ["HI", "CA", "TX", "SC", "KY", "MS"]
-nicotine_status = ["Never Used", "Current user"]
+term_lengths = [10, 15, 20, 25, 30]
+nicotine_status = ["Smoker", "Non-Smoker"]
 ages = [20, 30, 40, 45, 50, 60, 65, 70]
 genders = ["Male", "Female"]
