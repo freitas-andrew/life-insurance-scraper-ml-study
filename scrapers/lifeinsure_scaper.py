6a0a8d1e30cf4c86c520d721ce785938cb0871b9
freitas-andrew <freitas_andrew@outlook.com>
2025-04-20 21:37:38 +0100

    Removed states, as have no effect on quotes, otherwise general bug fixing and clean up.

--- scrapers/lifeinsure_scaper.py
+++ scrapers/lifeinsure_scaper.py
@@ -1,114 +1,133 @@
 # Standard libraries
 import csv
-import time
 from datetime import datetime
+from pathlib import Path
 
 # Third-party libraries
 from selenium.webdriver.common.by import By
 
 # Local imports
-from utils import * # Imports init_driver, edit_page_context, select_dropdown, select_checkbox and entire data sample
+from utils import * # Imports init_driver, edit_page_context, select_dropdown, select_checkbox, ensure_page_ready and entire data sample
 
 def main():
-    quote_url = "https://quoter.lifeinsure.com/results?quote=fbSu2A4b44Pfg7WkYqUiBx2KPfVafAUM" # lifeinsure quote w/preset BMI, Health Rating and 1st Jan DOB
-
+    # lifeinsure quote w/preset BMI, Health Rating, 1st Jan DOB and State as Hawaii (upon analysis, state does not affect premiums)
+    quote_url = "https://quoter.lifeinsure.com/results?quote=fbSu2A4b44Pfg7WkYqUiBx2KPfVafAUM" 
+    
+    nicotine_status = ["Current user", "Never Used"] # Required format for matching later
+    
+    # Using global to avoid UnboundLocalError when modifying imported variable
+    global term_lengths
+    term_lengths = [f"{term} Year Term" for term in term_lengths] # Required format for matching later
+    
     driver = init_driver() # Initialising driver 
     with driver:
-        driver.get(quote_url)
-        results, failed = scrape_combinations(driver, coverage_amounts, term_lengths, ages, genders, nicotine_status, states)
+        results, failed = scrape_combos(driver, quote_url, coverage_amounts, term_lengths, ages, genders, nicotine_status)
         
-        # Retry a maximum of 5 times until success
-        for _ in range(5):
+        # Retry a maximum of 10 times until success
+        for i in range(1, 11):
             if not failed:
                 break
-            print("🔁 Retrying failed combos...")
+            print(f"🔁 Retrying failed combos... ({i})")
             new_failed = []
             for combo in failed:
-                driver.get(quote_url)  # ← reloading the page
                 # Unpacks each item in "combo" into separate single-item lists for passing to scrape_combinations
-                partial_results, partial_failed = scrape_combinations(driver, *[[x] for x in combo]) 
-                print(f"After retry: {partial_results}")
-                results.extend(partial_results)      # Adding to results
-                new_failed.extend(partial_failed)    # Collecting still-failing combos
-            failed = new_failed                      # Retry remaining ones next round
-            
+                retry_results, retry_failed = scrape_combos(driver, quote_url, *[[x] for x in combo])
+                results.extend(retry_results)      # Adding to results
+                new_failed.extend(retry_failed)    # Collecting still-failing combos
+            failed = new_failed                    # Retry remaining ones next round
             
     file_name = "US_quotes.csv"
-    with open(file_name, mode="w", newline="") as file:
+    # Explicitly stating file path (../data/raw/{file_name} is done relative to console's current directory)
+    output_path = Path(__file__).resolve().parent.parent / "data" / "raw" / file_name
+    # Saving to CSV
+    with open(output_path, mode="w", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow(["Coverage Amount", "Term Length", "Age", "Gender", "Is_Smoker", "State", "Premium"])
+        writer.writerow(["Coverage Amount", "Term Length", "Age", "Gender", "Is_Smoker", "Premium"]) # CSV header
         writer.writerows(results)
-
-    print(f"📁 All done — exported to '{file_name}'")
+    print(f"📁 All done — exported to '{file_name}', in {output_path}")
     
     
-def scrape_combinations(driver, coverage_amounts, term_lengths, ages, genders, nicotine_status, states):
-    """
-    Iterates through combinations of inputs and collects premiums.
+def scrape_combos(driver, quote_url, coverage_amounts, term_lengths, ages, genders, nicotine_status):
+    """Iterates through combinations of inputs and collects premiums.
 
+    Args:
+        quote_url (str): URL of quote website (has premiums and risk editing on same page)
+        var_name (list): takes in multiple lists of variables to form each combo, and collect their premiums.
+        
     Returns:
-        List of tuples containing input parameters and corresponding premiums.
+        all_data (list): List of tuples containing input parameters and corresponding premiums.
+        failed_combos (list): list of tuples containing input parameters for which scraping failed.
     """
     all_data = []
     failed_combos = []
-    time.sleep(1)
+    
+    driver.get(quote_url) # Reloading the page
+    ensure_page_ready(driver, xpath="div[x-show='loading && !resultsModalOpen']") # Waiting for page to load
 
     # Iterating through each possible combo of variables
     for coverage in coverage_amounts:
         for term in term_lengths:
             for age in ages:
                 for gender in genders:
-                    for r_nicotine in nicotine_status:
-                        for state in states:
-                            # Calculating birth year corresponding to given age (using 1st of Jan as baseline)
-                            birth_year = str(datetime.now().year - age)
-                            try:
-                                # Selecting coverage amount and term length
-                                select_dropdown(driver, field_name="coverage_amount", value=coverage)
-                                select_dropdown(driver, field_name="category_code", value=term, by_visible_text=True)
-                                # Opening the edit page and selecting relevant dropdowns for current combo
-                                with edit_page_context(driver, edit_text='Edit'):
-                                    select_dropdown(driver, field_name="state", value=state)
-                                    select_dropdown(driver, field_name="tobaccotime", value=r_nicotine, by_visible_text=True)
-                                    select_dropdown(driver, field_name="dob_year", value=birth_year)
-                                    select_checkbox(driver, option_text=gender)
-                                    
-                                # Extracting each premium corresponding to this combo profile
-                                premiums = extract_premiums(driver)
-                                
-                                # Appending data to our list (if premiums is empty nothing is appended)
-                                for premium in premiums:
-                                    all_data.append((coverage, term, age, gender, r_nicotine, state, premium))
+                    for nic in nicotine_status:
+                        # Calculating birth year corresponding to given age (using 1st of Jan as baseline)
+                        birth_year = str(datetime.now().year - age)
+                        try:
+                            # Selecting coverage amount and term length
+                            select_dropdown(driver, field_name="coverage_amount", value=coverage)
+                            ensure_page_ready(driver, xpath="div[x-show='loading && !resultsModalOpen']")
+                            # Scroll to top to make sure term dropdown is visible
+                            driver.execute_script("window.scrollTo(0, 0);")
+                            select_dropdown(driver, field_name="category_code", value=term, by_visible_text=True)
+                            ensure_page_ready(driver, xpath="div[x-show='loading && !resultsModalOpen']")
+                            # Opening the edit page and selecting relevant dropdowns for current combo
+                            with edit_page_context(driver, edit_node="a", edit_text="Edit", btn_node="button"):
+                                select_dropdown(driver, field_name="tobaccotime", value=nic, by_visible_text=True)
+                                select_dropdown(driver, field_name="dob_year", value=birth_year)
+                                select_checkbox(driver, xpath=f"//*[normalize-space(text())='{gender}']")
+                            ensure_page_ready(driver, xpath="div[x-show='loading && !resultsModalOpen']")
+                            # Extracting each premium corresponding to this risk profile
+                            premiums = extract_premiums(driver)
+                            
+                            # Appending data to our list (if premiums is empty nothing is appended)
+                            for premium in premiums:
+                                all_data.append((coverage, term, age, gender, nic, premium))
 
-                                print(f"✅ Done: {coverage}, {term}: Age {age}, {gender}, {r_nicotine}, {state} ") # Printing current combo completed
-                            except Exception as e:
-                                print(f"❌ Error on combo: {coverage}, {term}: Age {age}, {gender}, {r_nicotine}, {state}: {e}")
-                                failed_combos.append((coverage, term, age, gender, r_nicotine, state)) # Recording failed combos
-                                continue
+                            if premiums:
+                                print(f"✅ Done: {coverage}, {term} Year Term: Age {age}, {gender}, {nic}") # Printing current combo completed
+                            else:
+                                print(f"⏭️  Skipping: No quotes found for {coverage}, {term}, Year Term: Age {age}, {gender}, {nic}")
+                                
+                        except Exception as e:
+                            print(f"❌ Error on combo: {coverage}, {term}: Age {age}, {gender}, {nic}: {e}")
+                            failed_combos.append((coverage, term, age, gender, nic)) # Recording failed combos
+                            continue
     return all_data, failed_combos
 
 
 def extract_premiums(driver):
-    """
-    Extracts available premiums from the 'No Medical Exam Policies' section.
+    """Extracts available premiums from the 'No Medical Exam Policies' section.
 
     Returns:
-        List of multiple premium strings (e.g., ['$14.32']) for each combo.
+        premiums: List of multiple premium strings (e.g., ['$14.32']) for each combo.
     """
     premiums = []
-    time.sleep(1)
+    
+    no_results_elem = driver.find_element(By.XPATH, "//h2[contains(text(), 'Your original search had no results')]")
+    # If 'no results' element is visibly displayed return empty premiums list
+    if no_results_elem.is_displayed():
+        return premiums
+    
     try:
-        no_me_header = driver.find_element(By.XPATH, "//div[contains(text(), 'No Medical Exam Policies')]") # Find the header 'No Medical Exam Policies'
+        # Find the header 'No Medical Exam Policies'
+        no_me_header = driver.find_element(By.XPATH, "//div[contains(text(), 'No Medical Exam Policies')]") 
         no_me_section = no_me_header.find_element(By.XPATH, "..") # Go to the parent div
     except:
-        print("⏭️ Skipping: No quotes found.") # If header isn't present, implies no quotes under no_me
-        return premiums
+        return premiums # Return nothing, as if header isn't present, implies no quotes under no_me
 
-    # Try to expand more results
+    # Try to expand more results by clicking "View more" button
     try:
-        show_more_button = no_me_section.find_element(By.XPATH, ".//a[contains(text(), 'View')]")
-        show_more_button.click()
-        time.sleep(0.5)
+        no_me_section.find_element(By.XPATH, ".//a[contains(text(), 'View')]").click()
     except:
         pass  # Ignore if "View more" button not present
 
@@ -116,7 +135,7 @@ def extract_premiums(driver):
     dollar_spans = no_me_section.find_elements(By.XPATH, ".//span[@x-text=\"getModalPrice(row, paymentMode).split('.')[0]\"]")
     cent_spans = no_me_section.find_elements(By.XPATH, ".//span[@x-text=\"getModalPrice(row, paymentMode).split('.')[1]\"]")
 
-# Pairs dollar and cent values, formats them as "$d.c", and filters out empty values
+    # Pairs dollar and cent values, formats them as "$d.c", and filters out empty values
     premiums = [
         f"${d}.{c}"
         for d, c in zip(
